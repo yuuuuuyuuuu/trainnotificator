b406c9fdb17186fc1759a18e239150662c03c832
YuuuuuYuuuuu <yuu.android.dev@gmail.com>
2014-08-31 19:46:15 +0900

    :new: add notification manager, time table model

---
+++ TrainNotificator/app/src/main/java/com/dev/android/yuu/trainnotificator/NotificationAlarmManager.java
@@ -0,0 +1,17 @@
+package com.dev.android.yuu.trainnotificator;
+
+import android.content.Context;
+
+/**
+ * Created by Chieko on 8/31/14.
+ */
+public class NotificationAlarmManager
+{
+    private Context mContext = null;
+
+    public NotificationAlarmManager(Context context)
+    {
+        this.mContext = context;
+    }
+
+}
---
+++ TrainNotificator/app/src/main/java/com/dev/android/yuu/trainnotificator/TrainTimeTableModel.java
@@ -0,0 +1,13 @@
+package com.dev.android.yuu.trainnotificator;
+
+/**
+ * Created by Chieko on 8/31/14.
+ */
+public class TrainTimeTableModel
+{
+    public  TrainTimeTableModel()
+    {
+        
+    }
+
+}
